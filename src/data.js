ea28ab28795f8647c7dc13a6e1647f5699de6a9e
alejandraglezd <alejandra.gnzlz.dz@gmail.com>
2021-08-02 10:36:39 -0500

    agrega funcion

--- src/data.js
+++ src/data.js
@@ -1,9 +1,22 @@
+/*Manipulación de datos a traves de arreglos y objetos (lógica)*/
 // estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const pokemonType = type.filter(x =>) => {
+    return 'example';
 };
 
 export const anotherExample = () => {
-  return 'OMG';
+    return 'OMG';
 };
+
+
+/*Posible filtro para obtener tipos de pokemones
+
+let allPokemon = [{data.pokemon}];
+let pokemonType == type.filter(function(type){
+    return pokemonType == "pokemon-type"; });
+console.log(pokemon-type);
+})
+*/
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
+/*Manipulación del DOM*/
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './data/pokemon/pokemon.js';
+/*data.forEach()
+innerHTML*/
+
+const pokemon = data.pokemon;
+console.log(pokemon);
+
+let allPokemon = "" 
+pokemon.forEach(cadaPokemon =>{
+primeraHistoria += primeraPantalla (cadaPokemon)
+})
+
+
+function primeraPantalla (categoriaPokemon){
+    return `<div class = "box">
+        <img src="${}">
+    <h1>NOMBRE:${categoriaPokemon.name}</h1>
+    <p>NÚMERO:</p>
+    <p>PESO/ALTURA:</p>
+    <p>ATAQUE/DEFENSA:</p>
+    </div>`
+}
+
+document.getElementByID("root").innerHTML=primeraHistoria
+
+
+
