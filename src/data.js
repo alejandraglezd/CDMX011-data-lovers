5782e337efb422dbc758f211bc8345218119189d
alejandraglezd <alejandra.gnzlz.dz@gmail.com>
2021-08-24 21:03:38 -0500

    test branch 50

--- src/data.js
+++ src/data.js
@@ -1,36 +1,37 @@
 
-export function filterByType(allPokemon, pokemonType) {
+export const filterByType = (allPokemon, pokemonType) => {
     return allPokemon.filter(function (allPokemon) {
         return allPokemon.type.includes(pokemonType)
     })
 }
 
-export function filterByGeneration(allPokemon, pokemonGeneration) {
+export const filterByGeneration = (allPokemon, pokemonGeneration) => {
     return allPokemon.filter(function (allPokemon) {
         return allPokemon.generation.name.includes(pokemonGeneration)
     })
 }
 
-export function alphabeticOrder(allPokemon, condition) {
-    let result = ""
+export const alphabeticOrder = (allPokemon, condition) => {
       if (condition === "A-Z") {
-        result = allPokemon.sort(function(a, b){
+        return allPokemon.sort(function(a, b){
           if (a.name > b.name) {
             return 1
-          } if(a.name<b.name){
+          } 
+          if(a.name < b.name){
             return -1
           }
           return 0
         });
       } else if(condition ==="Z-A"){ 
-        result = allPokemon.sort((a, b) => {
+        return allPokemon.sort((a, b) => {
           if (a.name < b.name) {
             return 1
-          } if (a.name>b.name) {
+          } 
+          if (a.name > b.name) {
             return -1
             }
-            return 0
+           return 0
         })
       }
-      return result
+     
     }
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,20 +1,54 @@
-//import { example, anotherExample } from '../src/data.js';
+import {pokemonMock, bulbasaurMock}  from './dataMock.js'
+import {filterByType, alphabeticOrder, filterByGeneration } from '../src/data.js';
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+const allPokemon = pokemonMock;
+const grassPokemon = bulbasaurMock;
+
+
+
+describe('filtrado por tipo de pokemon', () => {
+
+  it('test de filtrado por tipo', () => {
+    expect(filterByType(allPokemon, 'grass')).toHaveLength(1);
   });
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
+  it('test que valida que sea una función', () => {
+    expect(typeof filterByType).toBe('function');
+  })
+  it('debe retornar un pokemon de pasto', () => {
+    expect(filterByType(allPokemon, 'grass')).toEqual(grassPokemon)
+  })
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+describe('filtrado por generación de pokemon', () => {
+  
+  it('test de filtrado por generación', () => {
+    expect(filterByGeneration(allPokemon, 'kanto')).toHaveLength(3);
+  })
+  it('test que valida que sea una funcion', () => {
+    expect(typeof filterByGeneration).toBe('function');
+  })
+  // it('debe retornar un pokemon de generacion kanto', () => {
+  //   expect(filterByGeneration(allPokemon, '')).toEqual(grassPokemon);
+  // })
+})
+
+
+
+
+
+describe('filtrado por orden alfabetico pokemon', () => {
+  it('debe retornar pokemones ordenados A-Z', () => {
+    const sortPokemons = alphabeticOrder(allPokemon, 'A-Z');
+    expect(sortPokemons).toHaveLength(3);
+    expect(sortPokemons[0]).toEqual(bulbasaurMock[0]);
+  })
+  it('debe retornar pokemones ordenados Z-A', () => {
+    const sortPokemons = alphabeticOrder(allPokemon, 'Z-A');
+    expect(sortPokemons).toHaveLength(3);
+    expect(sortPokemons[3]).toEqual(bulbasaurMock[3]);
+  })
+  it('', () => {
+    
+  })
 });
